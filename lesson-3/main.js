05fea20bfd2e138573d9755cdab1720f0c667241
Ayatullo <aatullorahmatullaev@gmail.com>
2023-12-04 09:38:54 +0000

    first commit

---
+++ lesson-3/main.js
@@ -0,0 +1,24 @@
+// Комментариии линейный
+/*
+Комментариии
+
+многострочный
+*/
+/**/
+// Условные конструкции (if else жана switch case default)
+
+
+
+
+let color = 'green'
+
+if (color === 'red') {
+    console.log("STOP!");
+    document.write("<h4> STOP! </h4>")
+
+}
+
+if (color === 'green') {
+    console.log("GO->");
+    document.write("<h4> GOO! </h4>")
+}
